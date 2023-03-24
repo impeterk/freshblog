b82cba6b6ffc177e7ecef0b96e0150483119ca2c
Peter <storm.sk123@gmail.com>
2023-03-24 23:46:41 +0100

    added listPosts to main page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,34 @@
 import { h } from "preact";
 import { tw } from "twind";
+import { listPosts, Post } from "../utils/posts.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
 
-export default function Home() {
+export const handler: Handlers<Post[]> = {
+  async GET(_req, ctx) {
+    const posts = await listPosts();
+    return ctx.render(posts);
+  },
+};
+
+export default function Home(props: PageProps<Post[]>) {
+  const posts = props.data;
   return (
     <>
-      <p class={tw`p-4 mx-auto max-w-screen-md`}>Hello world</p>
+      <div class={tw`p-4 mx-auto mt-4 max-w-screen-md`}>
+        <h1 class={tw`text-5xl mt-12 font-bold`}>Peter's Blog</h1>
+        <ul class={tw`mt-8`}>
+          {posts.map((post) => <PostEntry post={post} />)}
+        </ul>
+      </div>
     </>
+  );
+}
+
+function PostEntry(props: { post: Post }) {
+  const post = props.post;
+  return (
+    <li>
+      <h2 class={tw`text-2xl text-red`}>{post.title}</h2>
+    </li>
   );
 }
