98e3d5804149695f1d46d501b48b756dc7ef6594
Peter <storm.sk123@gmail.com>
2023-03-25 13:06:00 +0100

    added 404 page added twind colors

--- routes/blog/[id].tsx
+++ routes/blog/[id].tsx
@@ -1,29 +1,25 @@
-import { h } from "preact";
-import { tw } from "twind";
-import { Handlers, PageProps } from "$fresh/server.ts"
-import { Post, loadPost } from "../../utils/posts.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { loadPost, Post } from "../../utils/posts.ts";
 
 export const handler: Handlers<Post> = {
   async GET(req, ctx) {
-    const id = ctx.params.id
-    const post = await loadPost(id)
-    if (!post) {
-      return new Response("Post not found", {status: 404})
-    }
+    const id = ctx.params.id;
+    const post = await loadPost(id);
+    if (!post) return ctx.renderNotFound();
     return ctx.render(post);
-  }
-}
+  },
+};
 
 export default function BlogPost(props: PageProps<Post>) {
-  const post = props.data
+  const post = props.data;
   return (
     <>
-      <div class={tw`p-4 mx-auto mt-4 max-w-screen-md`}>
-        <p class={tw`mt-8`}>
+      <div class="p-4 mx-auto mt-4 max-w-screen-md">
+        <p class="mt-8">
           {post.publishat.toLocaleDateString()}
         </p>
-        <h1 class={tw`text-5xl mt-4 font-bold`}>{post.title}</h1>
-        <div class={tw`mt-12`}>
+        <h1 class="text-5xl mt-4 font-bold">{post.title}</h1>
+        <div class="mt-12">
           {post.snippet}
           {post.content}
         </div>
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,3 @@
-import { h } from "preact";
-import { tw } from "twind";
 import { listPosts, Post } from "../utils/posts.ts";
 import { Handlers, PageProps } from "$fresh/server.ts";
 
@@ -14,21 +12,28 @@ export default function Home(props: PageProps<Post[]>) {
   const posts = props.data;
   return (
     <>
-      <div class={tw`p-4 mx-auto mt-4 max-w-screen-md`}>
-        <h1 class={tw`text-5xl mt-12 font-bold`}>Peter's Blog</h1>
-        <ul class={tw`mt-8`}>
+      <div class="p-4 mx-auto mt-4 max-w-screen-md">
+        <h1 class="text-5xl mt-12 font-bold">Peter's Blog</h1>
+        <ul class="mt-8">
           {posts.map((post) => <PostEntry post={post} />)}
         </ul>
       </div>
     </>
   );
 }
-
 function PostEntry(props: { post: Post }) {
   const post = props.post;
   return (
-    <li>
-      <h2 class={tw`text-2xl text-red`}>{post.title}</h2>
+    <li class="border-t py-4 mt-4">
+      <a class="flex gap-4 ml-8" href={`/blog/${post.id}`}>
+        <p class="text-base">
+          {post.publishat.toLocaleDateString("sk-SK")}
+        </p>
+        <div class="">
+          <h2 class="text-2xl text-sky-800">{post.title}</h2>
+          <p class="italic text-gray-600">{post.snippet}</p>
+        </div>
+      </a>
     </li>
   );
 }
